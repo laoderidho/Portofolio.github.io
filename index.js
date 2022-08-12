d7398e2fbf60ba253ad1ca68684166ee49bb7d79
laoderidho <2081007@unai.edu>
2022-08-12 12:07:17 +0700

    update

--- index.js
+++ index.js
@@ -2,5 +2,5 @@ const icon = document.querySelector('.icon');
 const nav = document.querySelector('nav ul');
 
 icon.addEventListener('click', function(){
-    nav.classList.toggle('slide')
+    nav.classList.toggle('slide');
 })
