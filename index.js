e28418a89d382688185cde5367c70578851b96e6
laoderidho <2081007@unai.edu>
2022-11-25 19:42:22 +0700

    update today

--- index.js
+++ index.js
@@ -1,4 +1,4 @@
 window.addEventListener('scroll', ()=>{
-    const head = document.querySelector('header')
+    const head = document.querySelector('nav')
     head.classList.toggle('sticky', window.scrollY > 0);
 })
